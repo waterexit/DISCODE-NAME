820aa4fe5f4f4ba3b01f1969808c70168a67d0a1
water <waterexit1120@gmail.com>
2022-02-20 22:16:34 +0900

    mend

--- game/test.js
+++
@@ -1,7 +0,0 @@
-import { promises } from 'fs';
-
-let data = await promises.readFile("words/words.txt", "utf-8");
-data = data.replace(/\n/g, 'a');
-
-let words = data.split(',');
-console.log(words);
